a7010fb25560a3f843ed0be164ffa20585d365d2
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-11-09 23:23:48 +0900

    5강 - Counter Header, cpp 파일 수정 (코멘트, 코드 추가)

--- practice/header/Counter_1.cpp
+++ practice/header/Counter_1.cpp
@@ -10,19 +10,19 @@ using namespace std;
 /**
  * Counter_1.p 파일의 Test Main File
  */
-int main() {
-    Counter_1 c1;
-    c1.reset();
-
-    cout << "before Value = " << c1.getValue() << endl;
-
-    c1.count();
-    c1.count();
-
-    cout << "after Value = " << c1.getValue() << endl;
-
-    c1.reset();
-
-    cout << "reset Value = " << c1.getValue() << endl;
-
-}
+//int main() {
+//    Counter_1 c1;
+////    c1.reset();
+//
+//    cout << "before Value = " << c1.getValue() << endl;
+//
+//    c1.count();
+//    c1.count();
+//
+//    cout << "after Value = " << c1.getValue() << endl;
+//
+//    c1.reset();
+//
+//    cout << "reset Value = " << c1.getValue() << endl;
+//
+//}
--- practice/header/Counter_1.h
+++ practice/header/Counter_1.h
@@ -18,13 +18,38 @@
  *
  * -> 즉 해당 header 파일의 내용을 정의하는 것.
  *
+ * header 파일의 Class 내부에 메서드가 동작부가 완성되지 않았다면,
+ * 같은 이름의 .cpp 파일내에 동작 로직을 구현해줄 수 있다.
+ * 그럴 경우 (클래스 멤버 정의?)
+ *      -> void {className}::{method Name}() {...} 으로 구현 부를 만들어 줄 수 있다.
+ *  -> 이경우 header 파일만 include 하여 사용해도 구현부 까지 사용 가능하다. (구현부는 .cpp 파일에 있다)
+ *
+ *
+ * 생성자의 초기화 리스트
+ *  -> 생성자의 머리 부분에 데이터 멤버를 초기화 하는 값들을 나열한 리스트
+ *  일반적으로 생성자 내부에 data = 0; 으로 초기화 시키지만
+ *  Counter_1() : value{0} {} 으로 데이터를 초기화 시킬 수 있음.
+ *  -> 이 경우 const 변수 또한 초기화 시킬 수 있음.
+ *
+ *  *소멸자란(destructor)
+ *  -> 객체가 소멸될 떄 수행할 작업을 정의한 특수한 함수
+ *      -> 클래스 이름에 '~'를 붙여 선언
+ *      -> 파라미터 X
+ *      -> 일반적으로 public 으로 선언함.
+ *      -> 상속을 통해 파생 클래스 정의할 경우 virtual 을 지정해 가상함수가 되도록 하는게 좋음.
+ *
  * #pragma once 또한 통용됨.
  */
 class Counter_1 {
 
-    int value; //CLass 의 default 는 private . -> 구조체의 경우 public
+    int value; // Class 의 default 는 private . -> 구조체의 경우 public
 
 public:
+    // Constructor
+    Counter_1() : value{0} {
+        //value = 0; // 초기화 리스트 사용함
+    }
+
     void reset(){
         value = 0;
     };
@@ -35,6 +60,12 @@ public:
     // const 키워드: 데이터의 값을 변경X -> ReadOnly
     int getValue() const {
         return value;
+    }
+
+    // 소멸자(destructor)
+    // 객체 제거를 위한 정리 작업을 주로함 ?
+    ~Counter_1() {
+        std::cout << "Counter_1.h Destructed" << std::endl;
     }
 
 };
