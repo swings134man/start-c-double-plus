dcc442a0ffad576319f8480251c54113c25e1bff
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-11-10 23:08:34 +0900

    main 주석처리

--- practice/header/person_test/PersonMain.cpp
+++ practice/header/person_test/PersonMain.cpp
@@ -10,21 +10,21 @@
 #include "Person.h"
 using namespace std;
 
-int main() {
-    Person* p1 = new Person("김김김", "서울");
-    Person* p2 = new Person("박박박", "부산");
-
-    // p1,p2 는 포인터고, 엑세스 하기 위해서는 (*p1).print() 이지만 아래와 같이 엑세스 허용
-    p1 -> print();
-    p2 -> print();
-
-    cout << endl <<"주소 변경 :";
-    p2 ->chAddr("대전");
-    p2->print();
-
-    // 포인터 제거
-    delete p1;
-    delete p2;
-
-    return 0;
-};
+//int main() {
+//    Person* p1 = new Person("김김김", "서울");
+//    Person* p2 = new Person("박박박", "부산");
+//
+//    // p1,p2 는 포인터고, 엑세스 하기 위해서는 (*p1).print() 이지만 아래와 같이 엑세스 허용
+//    p1 -> print();
+//    p2 -> print();
+//
+//    cout << endl <<"주소 변경 :";
+//    p2 ->chAddr("대전");
+//    p2->print();
+//
+//    // 포인터 제거
+//    delete p1;
+//    delete p2;
+//
+//    return 0;
+//};
